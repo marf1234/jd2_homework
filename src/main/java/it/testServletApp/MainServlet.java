45367bd3acce75ee2f41749b4c88f223411f485b
marf1234 <andrei.veg@gmail.com>
2021-10-12 17:32:39 +0300

    task 8

---
+++ src/main/java/it/testServletApp/MainServlet.java
@@ -0,0 +1,26 @@
+package it.testServletApp;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MainServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        PrintWriter out = response.getWriter();
+        out.println ("<html><Head><title> First Servlet</title></head>");
+        out.println ("<body><h1> This is first Servlet</h1>");
+        out.println ("</body></html>");
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        doGet(request, response);
+    }
+}
