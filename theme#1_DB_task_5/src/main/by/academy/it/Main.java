66f450bcb0e9eb251474073f10eb369daef18caa
marf1234 <andrei.veg@gmail.com>
2021-10-26 00:13:31 +0300

    Task_5 done

---
+++ theme#1_DB_task_5/src/main/by/academy/it/Main.java
@@ -0,0 +1,16 @@
+package src.main.by.academy.it;
+
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class Main {
+
+
+    public static void main(String[] args) throws SQLException {
+        DataAccessObject dataAccessObject = new DataAccessObject();
+        dataAccessObject.addingDataToExpensesTable(args[0], args[1], args[2], args[3]);
+        dataAccessObject.outputTable();
+
+    }
+}
