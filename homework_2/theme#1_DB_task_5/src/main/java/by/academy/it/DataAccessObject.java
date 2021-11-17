c36cabe7eb07905d9c2637d005c6649b74a92df6
Андрей <andre.veg@gmail.com>
2021-11-17 18:18:10 +0300

    Task_4-7 project structure,package name fix

---
+++ homework_2/theme#1_DB_task_5/src/main/java/by/academy/it/DataAccessObject.java
@@ -0,0 +1,39 @@
+package by.academy.it;
+
+import java.sql.*;
+
+public class DataAccessObject {
+
+    private DataSource dataSource = new DataSource();
+
+    public void addingDataToExpensesTable(String num, String date, String receiver, String value) throws SQLException {
+
+        Connection connection = dataSource.getConnection();
+        String template = "INSERT INTO expenses (paydate, receiver, value) VALUES (?, ?, ?, ?);";
+        PreparedStatement preparedStatement = connection.prepareStatement(template);
+        preparedStatement.setString(1, num);
+        preparedStatement.setString(2, date);
+        preparedStatement.setString(3, receiver);
+        preparedStatement.setString(4, value);
+
+    }
+
+    public void outputTable() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT paydate, value, name FROM expenses, " +
+                "receivers WHERE receiver=receivers.num;");
+
+        while (resultSet.next()) {
+            System.out.println("| "
+                    + resultSet.getDate("paydate") + " | "
+                    + resultSet.getInt("value") + " | "
+                    + resultSet.getString("name") + " | "
+            );
+        }
+
+        statement.close();
+        connection.close();
+    }
+}
+
---
+++ homework_2/theme#1_DB_task_5/src/main/java/by/academy/it/Main.java
@@ -0,0 +1,15 @@
+package by.academy.it;
+
+
+import java.sql.SQLException;
+
+public class Main {
+
+
+    public static void main(String[] args) throws SQLException {
+        DataAccessObject dataAccessObject = new DataAccessObject();
+        dataAccessObject.addingDataToExpensesTable(args[0], args[1], args[2], args[3]);
+        dataAccessObject.outputTable();
+
+    }
+}
